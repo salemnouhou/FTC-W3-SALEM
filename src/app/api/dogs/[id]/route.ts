3e159906a8b578b753d8ec50be0bbd617c45abf7
Salem Souleymane NOUHOU <salem.nouhou@epitech.eu>
2025-09-06 00:12:33 +0100

    FIX/UPDATE SECOND ARGUMENT FOR ROUTE [ID]

--- src/app/api/dogs/[id]/route.ts
+++ src/app/api/dogs/[id]/route.ts
@@ -1,16 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import dogs from "../../../../../data/dogs.json";
 
+// ✅ Corrigé : params doit être dans le context
 export async function GET(
-    req: Request,
-    { params }: { params: { id: string } }
-  ) {
-    const dog = dogs.find((d) => d.id === Number(params.id));
-  
-    if (!dog) {
-      return NextResponse.json({ message: "Dog not found" }, { status: 404 });
-    }
-  
-    return NextResponse.json(dog);
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
+  const dog = dogs.find((d) => d.id === Number(id));
+
+  if (!dog) {
+    return NextResponse.json({ message: "Dog not found" }, { status: 404 });
   }
-  
+
+  return NextResponse.json(dog);
+}
--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,12 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest,NextResponse } from "next/server";
 import products from "../../../../../data/products.json";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
-  const product = products.find((p) => p.id === Number(params.id));
 
+  const { id } = context.params;
+  const product = products.find((p) => p.id === Number(id));
   if (!product) {
     return NextResponse.json({ message: "Product not found" }, { status: 404 });
   }
